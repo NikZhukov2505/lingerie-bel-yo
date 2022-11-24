2e65aa45dfd0a2aea5b44487e4352de7ac1a71df
NikZhukov2505 <unnormaldj@gmail.com>
2022-11-24 13:04:28 +0600

    css & route changes

--- Components/Pagination.js
+++ Components/Pagination.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import styles from '../styles/Paginator.module.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from './../redux/reducers/lingerie-reducer';
+import { useRouter } from 'next/router';
 
 const Paginator = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [pagesCount, setPagesCount] = useState(12)
     const { products, order, categoryName } = useSelector(state => state?.lingerie)
     const dispatch = useDispatch()
+    const router = useRouter()
+
 
     useEffect(() => {
         setCurrentPage(1)
@@ -16,6 +19,7 @@ const Paginator = () => {
 
     useEffect(() => {
         dispatch(getProducts(currentPage, order, categoryName))
+
     }, [currentPage])
 
     return (
@@ -23,7 +27,10 @@ const Paginator = () => {
             <Pagination
                 defaultPage={1}
                 page={currentPage}
-                onChange={(e, page) => setCurrentPage(page)}
+                onChange={(e, page) => {
+                    setCurrentPage(page)
+                    router.push(`/?page=${page}`, undefined, { shallow: true })
+                }}
                 count={Math.ceil(products?.count / pagesCount)} size="large" />
         </div>
     );
